8334027f590b1826dbea8775b777e57b5f4a7b59
VidalJames <cubero.vj@gmail.com>
2022-04-05 05:40:51 +0800

    third commit

---
+++ script.js
@@ -0,0 +1,15 @@
+window.addEventListener('resize', function() {
+//     if (window.screen.width < 1200 && window.screen.width > 600) {
+//         let windowScreen = window.screen.width / 2;
+//         let scale = windowScreen / 720;
+//         console.log(scale)
+
+//         document.querySelector(".mday-block-3-grid-box-quote").style.cssText = `
+//         transform: scale(${scale});
+//         -webkit-transform: scale(${scale});
+//         width: 100%;
+//         `
+//     } else {
+//         document.querySelector(".mday-block-3-grid-box-quote").style.cssText = {}
+//     }
+// })
